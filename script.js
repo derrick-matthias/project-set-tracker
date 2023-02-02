8fda368d157049c8ed59dc3c60a81b05ac10214c
Derrick Matthias <derricksmatthias@gmail.com>
2023-02-02 15:49:45 -0800

    Committing index, .js and .css files

---
+++ script.js
@@ -0,0 +1,46 @@
+// Namespace for this app structure:
+const app = {};
+
+//HTML Elements:
+
+// Setting global variables to be overwritten by the app later:
+
+// When user types in workouts in the search bar, GET workouts that match the user's input
+
+// Display search results to the user for them
+
+// The user needs to select a workout to open workout details
+
+// Display workout details to the user
+
+// When viewing a workout, the user must have an option to add the exercise to their workout
+
+// User must see their workouts on the app web page
+
+// User must be able to create, edit or remove a new workout
+
+// When creating a workout, the user must be able to search and add exercises
+
+// When adding an exercise, the user must be able to select the number of sets and reps per exercise
+
+// Users should be able to start a workout
+
+// When a workout is started, a timer is started
+
+// When a workout is active, the user must be able to select a workout set to complete
+
+// The app must display the previous number of sets the user completed from their previous workout
+
+// The user must be able to adjust their total reps and submit the reps completed
+
+// When a user completes a set, a rest timer must be dispalyed to the user
+
+// The user must be able to adjust or stop the rest timer
+
+// The user must be able to complete their workout
+
+// The app must prompt the user to save their workout data and adjust
+
+// When a workout is saved, their previous data on number of sets is visible in their workout
+
+// When a workout is saved,
